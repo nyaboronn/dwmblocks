5b6f48b8cd21316efc02e3cd4b4bfacc0196ae7b
Nyaboron <aliagahyderalejandro@gmail.com>
2025-09-21 17:45:15 +0200

    Uso del repo con los scripts

--- blocks.h
+++ blocks.h
@@ -2,8 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", 	 "~/scripts/dwmblocks_volumen", 							1, 		0},
+	{" ",   "date '+%d/%m/%Y - %H:%M'", 								5, 		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
