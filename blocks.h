1b5dbf899d5aafe2fe02fbfa1ce4eef7c8266cd7
Nyaboron <aliagahyderalejandro@gmail.com>
2026-09-07 20:18:52 +0200

    fix intervalo para la fecha

--- blocks.h
+++ blocks.h
@@ -3,9 +3,8 @@ static const Block blocks[] = {
     /*Icon*/    /*Command*/                                                              /*Update Interval*/ /*Update Signal*/
     {"󰍛 ",     "free -h | awk '/^Mem:/ {print $3 \" / \" $2}' | sed 's/Gi//g; s/Mi/M/g'", 30,                 0},
     {" ",      "~/scripts/dwmblocks_volumen",                                             1,                  0},
-    {" 󰃭 ",	"date '+%a %d/%m/%Y'",                               					   60,                 0},
+    {" 󰃭 ",	"date '+%a %d/%m/%Y'",                               					   5,                 0},
     {" 󰥔 ",    "date '+%H:%M'",                                                           5,                  0},
-	{"", 	   "~/scripts/dwmblocks_bateria", 							                  60, 	              0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
