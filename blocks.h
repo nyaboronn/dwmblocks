08994ce00a4fa7036e71d98dfcd91b0a1cccf6e8
Nyaboron <aliagahyderalejandro@gmail.com>
2026-09-08 16:30:25 +0200

    separar la información del pc en un script

--- blocks.h
+++ blocks.h
@@ -1,12 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/                                                              /*Update Interval*/ /*Update Signal*/
-    {"󰍛 ",     "free -h | awk '/^Mem:/ {print $3 \" / \" $2}' | sed 's/Gi//g; s/Mi/M/g'", 30,                 0},
+    {"", 	   "~/scripts/systemstats", 							                      5, 	              0},
     {" ",      "~/scripts/dwmblocks_volumen",                                             1,                  0},
-    {" 󰃭 ",	"date '+%a %d/%m/%Y'",                               					   5,                 0},
+    {" 󰃭 ",	"date '+%a %d/%m/%Y'",                               					   5,                  0},
     {" 󰥔 ",    "date '+%H:%M'",                                                           5,                  0},
 	{" ", 	   "~/scripts/dwmblocks_bateria", 							                  30, 	              0},
-
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
