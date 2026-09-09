f301cbd9dda043cd7dcf401c2e866210d448fe95
Nyaboron <aliagahyderalejandro@gmail.com>
2026-09-09 17:54:49 +0200

    add update signals

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/                                                              /*Update Interval*/ /*Update Signal*/
-    {"", 	   "~/scripts/systemstats", 							                      5, 	              0},
-    {" ",      "~/scripts/dwmblocks_volumen",                                             1,                  0},
-    {" 󰃭 ",	"date '+%a %d/%m/%Y'",                               					   5,                  0},
-    {" 󰥔 ",    "date '+%H:%M'",                                                           5,                  0},
-	{" ", 	   "~/scripts/dwmblocks_bateria", 							                  30, 	              0},
+    {"", 	   "~/scripts/systemstats", 							                      5, 	              1},
+    {" ",      "~/scripts/dwmblocks_volumen",                                             1,                  2},
+    {" 󰃭 ",	"date '+%a %d/%m/%Y'",                               					   5,                  3},
+    {" 󰥔 ",    "date '+%H:%M'",                                                           5,                  4},
+	{" ", 	   "~/scripts/dwmblocks_bateria", 							                  30, 	              5},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
